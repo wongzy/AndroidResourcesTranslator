8881914c744315970275b8b2383707677c2d4e56
wongzhenyu <wongzhenyu96@gmail.com>
2020-12-01 19:56:51 +0800

    parse json

--- script/main.py
+++ script/main.py
@@ -1,5 +1,20 @@
+import json
+
 from googletrans import Translator
 from script.resource_constants import RESOURCE_SPECIAL_MAP, COMMON_PREFIX
 
 
+# read json config
+def read_config():
+    with open('../translate_config.json') as json_file:
+        json_config = json.load(json_file)
+    return json_config
 
+
+config = read_config()
+print(config["source_language"])
+print(config["source_path"])
+file_list = config["files_names"]
+print(file_list)
+language_list = config["translate_languages"]
+print(language_list)
